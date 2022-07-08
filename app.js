ff39773bd3fc5abd3314490139354dca062fae2b
t2n <t2n2004@gmail.com>
2022-07-08 23:22:06 +0700

    Chuyển hướng request

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/') {
+
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/> <button type="submit">Send</button> </form></body>')
+        res.write('</html>');
+        return res.end();
+
+    }
+
+
     console.log(req.url, req.method, req.headers);
     //process.exit();
     res.setHeader('Content-Type','text/html');
