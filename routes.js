9df794c29cd145a1a9d1e8a3ff67de4afd94e2d7
t2n <t2n2004@gmail.com>
2022-07-23 00:36:07 +0700

    fix routes

--- routes.js
+++ routes.js
@@ -20,7 +20,7 @@ const requestHandler = (req,res) => {
             body.push(chunk);
         });
     
-        req.on('end',() => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
             fs.writeFile('message.txt', message, err => {
