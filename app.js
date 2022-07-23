252ff4eefa888ea79a417a8825c542d53c18cbfd
t2n <t2n2004@gmail.com>
2022-07-24 00:00:20 +0700

    Xử lý các Route khác nhau

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); //this allows the request to continue to next middleware in line
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>')
+    res.send('<h1>This is "Add Product" Page!</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
