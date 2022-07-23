0704423d122e3c9e4066e30217668ecbaf19b8bd
t2n <t2n2004@gmail.com>
2022-07-23 23:41:35 +0700

    Cài đặt Middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); //this allows the request to continue to next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    //...
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
