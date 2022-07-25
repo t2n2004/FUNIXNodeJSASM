1653ff9843f124e0a54a00e14751942b73c86940
t2n <t2n2004@gmail.com>
2022-07-25 21:26:51 +0700

    Trả lại trang 404

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,7 +13,7 @@ app.use('/admin', adminRouters);
 app.use(shopRouters);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 });
 
 app.listen(3000);
