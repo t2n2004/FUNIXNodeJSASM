82ef21c5eb91eddb3bf8d82507e3f33743fe35c5
t2n <t2n2004@gmail.com>
2022-07-25 21:47:20 +0700

    Sử dụng chức năng trợ giúp để điều hướng

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,14 @@ const path = require('path');
 const express = require('express');
 const { join } = require('path');
 
+const rootDiv = require('../util/path');
+
 const router = express.Router();
 
 // /admin/add-product --> GET
 router.get('/add-product', (req, res, next) => {
     console.log('In another middleware!');
-    res.sendFile(path.join(__dirname,'../','views','/add-product.html'));
+    res.sendFile(path.join(rootDiv,'views','/add-product.html'));
 });
 
 
