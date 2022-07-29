7955549ef0b49dae1bcea6679d9ae20e352700da
t2n <t2n2004@gmail.com>
2022-07-30 00:56:52 +0700

    Xuất nội dung động

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,15 @@
 const path = require('path');
 
 const express = require('express');
-const rootDiv = require('../util/path');
+
+const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('shop');
+  const products = adminData.products;
+  res.render('shop', {prods: products, docTitle: 'Shop'});
 });
 
 module.exports = router;
