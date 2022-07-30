bedf9b4572ba4f4a9a84b49cfd4f5818573db801
t2n <t2n2004@gmail.com>
2022-07-30 21:42:12 +0700

    Hoàn thiện mẫu Pug

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,16 +9,16 @@ app.set('view engine', 'pug');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
-const shopRouters = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
-app.use(shopRouters);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
