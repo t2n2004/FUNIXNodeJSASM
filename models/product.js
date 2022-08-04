9cf02d2eadc77838a5d18da55e4ca3e9945eef40
t2n <t2n2004@gmail.com>
2022-08-04 23:43:48 +0700

    Lưu trữ dữ liệu trong tệp thông qua model

--- models/product.js
+++ models/product.js
@@ -2,33 +2,39 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        const p = path.join(path.dirname(process.mainModule.filename),
-        'data', 
-        'products.json'
-        );
-        fs.readFile(p,(err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        });
-    }
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll() {
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                return [];
-            }
-            return JSON.parse(fileContent);
-        });
-    }
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
+  }
 };
