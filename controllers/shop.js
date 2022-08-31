38964bb7ae3a11ce2b4baccf2f7776858e16f99f
t2n <t2n2004@gmail.com>
2022-08-31 23:29:03 +0700

    Thực hành Tìm nạp sản phẩm

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,15 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
@@ -25,7 +27,7 @@ exports.getProduct = (req, res, next) => {
 exports.getIndex = (req, res, next) => {
   Product.fetchAll()
     .then(([rows, fieldData]) => {
-      res.render('shop/product-list', {
+      res.render('shop/index', {
         prods: rows,
         pageTitle: 'Shop',
         path: '/'
