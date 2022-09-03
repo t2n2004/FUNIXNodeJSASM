95bd4b98793c99095b197908fb266d8fc02de481
t2n <t2n2004@gmail.com>
2022-09-04 00:29:45 +0700

    Tìm nạp tất cả các sản phẩm

--- app.js
+++ app.js
@@ -11,29 +11,27 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-//const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  //User.findByPk(1)
-  //  .then(user => {
-  //    req.user = user;
-  //    next();
-  //  })
-  //  .catch(err => console.log(err));
+  // User.findByPk(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
   next();
-  
 });
 
-//app.use('/admin', adminRoutes);
-//app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 mongoConnect(() => {
   app.listen(3000);
-})
-
+});
