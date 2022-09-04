8ca1b334d469b33879e6fa01ceeb725d5a4f03dc
t2n <t2n2004@gmail.com>
2022-09-04 23:24:20 +0700

    Làm việc trên mặt hàng đơn đặt hàng trong giỏ hàng

--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;  // { items: []  }
+    this._id = id;
   }
 
   save() {
@@ -14,11 +16,26 @@ class User {
     return db.collection('users').insertOne(this);
   }
 
+  addToCart(product) {
+    //const cartProduct = this.cart.items.findIndex(cp => {
+    //  return cp._id === product._id;
+    //});
+    
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne({ _id: new ObjectId(userId) });
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
       .collection('users')
-      .findOne({ _id: new ObjectId(userId) })
+      .findOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+        )
       .then(user => {
         console.log(user);
         return user;
