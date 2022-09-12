c57a84d541958649e2aab6ebcbe3a505481e50b2
t2n <t2n2004@gmail.com>
2022-09-12 23:28:51 +0700

    Sử dụng Middleware Session

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({secret: 'my secret', resave: false, saveUninitialized: false})
+  session({ secret: 'my secret', resave: false, saveUninitialized: false })
 );
 
 app.use((req, res, next) => {
