09509e1c01ef25518f902bb212c227eb0f56c4a8
t2n <t2n2004@gmail.com>
2022-09-13 23:08:25 +0700

    Làm cho "Thêm vào giỏ hàng" hoạt động trở lại

--- app.js
+++ app.js
@@ -31,8 +31,21 @@ app.use(
     secret: 'my secret', 
     resave: false, 
     saveUninitialized: false, 
-    store: store })
+    store: store 
+  })
 );
+
+app.use((req, res, next) => {
+  if (! req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use((req, res, next) => {
   User.findById('63176c6761eb2bdadcc6eaa4')
