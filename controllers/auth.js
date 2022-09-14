7f949a61935d83e880d1280d3def870af75a695d
t2n <t2n2004@gmail.com>
2022-09-14 21:34:30 +0700

    Thêm chức năng đăng nhập

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,14 +19,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('63176c6761eb2bdadcc6eaa4')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email  : email})
   .then(user => {
-    req.session.isLoggedIn = true;
-    req.session.user = user;
-    req.session.save(err => {
-      console.log(err);
-      res.redirect('/');
-    });
+    if (!user) {
+      return res.redirect('/login');
+    }
+    bcrypt.compare(password, user.password)
+      .then(doMatch => {
+        if (doMatch) { 
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save(err => {
+            console.log(err);
+            res.redirect('/');
+          }); 
+        }
+        res.redirect('/login');
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect('/login');
+      });
+    
   })
   .catch(err => console.log(err));
 };
