31b24c6e3c09880ed9c0d43c3fd7d6e45a3b8303
t2n <t2n2004@gmail.com>
2022-09-14 23:05:39 +0700

    Cung cấp phản hồi của người dùng

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,45 +6,45 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    errorMessage: req.flash('error')
   });
 };
 
 exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
-    pageTitle: 'Signup',
-    isAuthenticated: false
+    pageTitle: 'Signup'
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  User.findOne({ email  : email})
-  .then(user => {
-    if (!user) {
-      return res.redirect('/login');
-    }
-    bcrypt.compare(password, user.password)
-      .then(doMatch => {
-        if (doMatch) { 
-          req.session.isLoggedIn = true;
-          req.session.user = user;
-          return req.session.save(err => {
-            console.log(err);
-            res.redirect('/');
-          }); 
-        }
-        res.redirect('/login');
-      })
-      .catch(err => {
-        console.log(err);
-        res.redirect('/login');
-      });
-    
-  })
-  .catch(err => console.log(err));
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        req.flash('error', 'Invalid email or password.');
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
