585110607a7c8071f219059e1440c2dfc1e775f6
t2n <t2n2004@gmail.com>
2022-09-14 21:16:45 +0700

    Mã hóa mật khẩu

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -33,20 +35,24 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  User.findOne({email: email}).then(userDoc => {
-    if (userDoc) {
-      res.redirect('/signup');
-    }
-    const user = new User({
-      email: email,
-      password: password,
-      cart: { items: [] } 
-    });
-    return user.save();
-  })
-  .then(result => {
-    res.redirect('/login');
-  })
+  User.findOne({email: email})
+    .then(userDoc => {
+      if (userDoc) {
+        res.redirect('/signup');
+      }
+      return bcrypt.hash(password, 12);
+    })
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] } 
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
   .catch(err => {
     console.log(err);
   });
