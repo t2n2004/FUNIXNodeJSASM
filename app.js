5167f6ebba8652d8eb50b16feabc121d5ec0f370
t2n <t2n2004@gmail.com>
2022-09-14 20:21:08 +0700

    Triển khai Luồng xác thực

--- app.js
+++ app.js
@@ -65,18 +65,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Nga',
-          email: 'nga@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
