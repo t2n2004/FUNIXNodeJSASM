3f2ad3bd92e6b4b6c00bc51df7f13ff9f6823ddf
t2n <t2n2004@gmail.com>
2022-09-14 22:19:07 +0700

    Sử dụng Middleware để bảo mật cho Router

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const mongoDBStore = require('connect-mongodb-session')(session);
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -12,7 +12,7 @@ const User = require('./models/user');
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/shop';
 
 const app = express();
-const store = new mongoDBStore({
+const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
@@ -27,28 +27,19 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({ 
-    secret: 'my secret', 
-    resave: false, 
-    saveUninitialized: false, 
-    store: store 
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
   })
 );
 
 app.use((req, res, next) => {
-  if (! req.session.user) {
+  if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
-
-app.use((req, res, next) => {
-  User.findById('63176c6761eb2bdadcc6eaa4')
     .then(user => {
       req.user = user;
       next();
--- controllers/admin.js
+++ controllers/admin.js
@@ -8,7 +8,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: req.session.isLoggedIn
   });
 };
 
@@ -52,7 +52,7 @@ exports.getEditProduct = (req, res, next) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -90,7 +90,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
