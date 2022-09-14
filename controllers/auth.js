7e6a834a0d3855e4e8a26fc2c1ed90250ea27eb5
t2n <t2n2004@gmail.com>
2022-09-14 21:21:29 +0700

    Thêm một cải tiến code nhỏ

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,27 +35,28 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  User.findOne({email: email})
+  User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
-        res.redirect('/signup');
+        return res.redirect('/signup');
       }
-      return bcrypt.hash(password, 12);
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+        });
     })
-    .then(hashedPassword => {
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-        cart: { items: [] } 
-      });
-      return user.save();
-    })
-    .then(result => {
-      res.redirect('/login');
-    })
-  .catch(err => {
-    console.log(err);
-  });
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
