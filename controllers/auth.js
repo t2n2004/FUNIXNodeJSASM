9f1eaa451c8269122cbd85eefcc316202868b695
t2n <t2n2004@gmail.com>
2022-09-15 23:50:54 +0700

    Thêm kiểm tra dữ liệu không đồng bộ

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,7 +65,7 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+  
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
@@ -75,29 +75,20 @@ exports.postSignup = (req, res, next) => {
       errorMessage: errors.array()[0].msg,
     });
   }
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'E-Mail exists already, please pick a different one.');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
-          return user.save();
-        })
-        .then(result => {
-          res.redirect('/login');
-        });
+
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] }
+      });
+      return user.save();
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .then(result => {
+      res.redirect('/login');
+    })
 };
 
 exports.postLogout = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -12,32 +13,38 @@ router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
 
 router.post(
-    '/signup', 
-    [
-        check('email')
-            .isEmail()
-            .withMessage('Please enter a valid email.')
-            .custom((value, {req}) => {
-                if (value === 'test@test.com') {
-                    throw new Error('This email address is forbidden.');
-                }
-                return true;
-            }),
-        body(
-                'password', 
-                'Please enter a password with only number and text and at least 5 characters.'
-            )
-            .isLength({ min: 5 })
-            .isAlphanumeric(),
-        body('confirmPassword')
-            .custom((value, {req}) => {
-                if (value !== req.body.password) {
-                    throw new Error('Password must be matched.');
-                }
-                return true;
-            })
-    ], 
-    authController.postSignup
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email address if forbidden.');
+        // }
+        // return true;
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject(
+              'E-Mail exists already, please pick a different one.'
+            );
+          }
+        });
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters.'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords have to match!');
+      }
+      return true;
+    })
+  ],
+  authController.postSignup
 );
 
 router.post('/logout', authController.postLogout);
